9d66b201e38afcd6ee7dd5935e758c8083b1b596
Yegor Bitensky <yegorbitensky@gmail.com>
2022-05-18 23:58:36 +0300

    Add react to webpack service

--- src/webpack/src/main.js
+++
@@ -1,1 +0,0 @@
-window.QWERTY = 10;
--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'production',
-  entry: '/src/main.js',
+  entry: '/src/main.jsx',
   output: {
     filename: 'main.min.js',
     path: '/static',
