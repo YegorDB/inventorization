b369cc579b3c83c60d8fc44fee14b3cca8f705b8
Yegor Bitensky <yegorbitensky@gmail.com>
2022-05-27 23:05:05 +0300

    Add urlencoded parse middleware

--- src/express/main.js
+++ src/express/main.js
@@ -16,6 +16,7 @@ app.set("view engine", "pug");
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/profile', profileRouter);
