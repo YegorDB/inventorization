223f06b95ce478b7ab000e80721698945f6e6c97
Yegor Bitensky <yegorbitensky@gmail.com>
2022-06-02 23:25:39 +0300

    Add express error handler

--- src/express/src/app.js
+++ src/express/src/app.js
@@ -27,6 +27,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${ PORT }`);
