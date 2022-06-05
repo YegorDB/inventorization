5823a80d02c9befab8da150c2e685ae050d2af59
Yegor Bitensky <yegorbitensky@gmail.com>
2022-06-05 21:32:26 +0300

    Add item model

--- src/express/src/db.js
+++ src/express/src/db.js
@@ -5,3 +5,14 @@ mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+var Schema = mongoose.Schema;
+
+var ItemModelSchema = new Schema({
+    _id: Number,
+    title: String,
+    count: Number
+});
+
+var ItemModel = mongoose.model('ItemModel', ItemModelSchema);
