d77ddd3410817d9fa375f0c9b10872edff9e356a
Yegor Bitensky <yegorbitensky@gmail.com>
2022-06-07 23:56:07 +0300

    Improve item model

--- src/express/src/db.js
+++ src/express/src/db.js
@@ -10,8 +10,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var Schema = mongoose.Schema;
 
 var ItemModelSchema = new Schema({
-    _id: Schema.Types.ObjectId,
-    title: String,
+    // _id: Schema.Types.ObjectId,
+    title: { type: String, maxlength: 100 },
     count: { type: Number, min: 0 }
 });
 
