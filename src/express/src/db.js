eb9822daede6955e5eb7a0b40631e6bca15835fc
Yegor Bitensky <yegorbitensky@gmail.com>
2022-06-14 23:30:47 +0300

    Add test group instance

--- src/express/src/db.js
+++ src/express/src/db.js
@@ -1,9 +1,24 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo/main_db');
+// var Group = require('./models/group');
+
+mongoose.connect('mongodb://root:example@mongo:27017/');
 mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// var group = new Group({
+//   name: 'test',
+// });
+//
+// group.save()
+//   .then(doc => {
+//     console.log('SAVED', doc);
+//   })
+//   .catch(err => {
+//     console.error('ERROR', err);
+//   });
 
 module.exports = db;
