c3ac8a644e36220981d3ef094f5e1500e2c20b82
Yegor Bitensky <yegorbitensky@gmail.com>
2022-06-26 20:56:53 +0300

    Add index page data

--- src/express/src/routes/index.js
+++ src/express/src/routes/index.js
@@ -1,13 +1,27 @@
+var async = require('async');
 var express = require('express');
+
+var Group = require('../models/group');
+var Item = require('../models/item');
+
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    pageTitle: "Inventorization | Index",
-    title: "Index"
+  async.parallel({
+      group_count: function(callback) {
+          Group.countDocuments({}, callback);
+      },
+      item_count: function(callback) {
+          Item.countDocuments({}, callback);
+      },
+  }, function(err, results) {
+      res.render('index', {
+        pageTitle: "Inventorization | Index",
+        title: "Index",
+        data: results,
+        error: err
+      });
   });
 });
-
 
 module.exports = router;
