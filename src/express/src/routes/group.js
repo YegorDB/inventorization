563c4728e7ad6eaa9ae570a1625912cf86d8dc64
Yegor Bitensky <yegorbitensky@gmail.com>
2022-07-01 23:56:49 +0300

    Add group data to group page

--- src/express/src/routes/group.js
+++ src/express/src/routes/group.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var Group = require('../models/group');
+
 
 router.get('/:groupId/', function(req, res, next) {
   console.log('groupId', req.params.groupId);
-  res.render('group', {
-    pageTitle: "Inventorization | Group",
-    title: "Group"
-  });
+
+  Group.findOne({ _id: req.params.groupId })
+    .exec(function (err, group) {
+      if (err) { return next(err); }
+
+      res.render('group', {
+        pageTitle: "Inventorization | Group",
+        title: "Group",
+        group: group
+      });
+    });
 });
 
 
