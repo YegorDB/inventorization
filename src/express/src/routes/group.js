43432e3639099e0732359c8345332327cfff7ff1
Yegor Bitensky <yegorbitensky@gmail.com>
2022-07-04 23:58:36 +0300

    Add items data to group page

--- src/express/src/routes/group.js
+++ src/express/src/routes/group.js
@@ -1,22 +1,30 @@
+var async = require('async');
 var express = require('express');
-var router = express.Router();
 
 var Group = require('../models/group');
+var Item = require('../models/item');
 
+var router = express.Router();
 
 router.get('/:groupId/', function(req, res, next) {
   console.log('groupId', req.params.groupId);
 
-  Group.findOne({ _id: req.params.groupId })
-    .exec(function (err, group) {
+  async.parallel({
+      group: function(callback) {
+          Group.findOne({ _id: req.params.groupId }).exec(callback);
+      },
+      items: function(callback) {
+          Item.find({ group: req.params.groupId }).exec(callback);
+      },
+  }, function(err, results) {
       if (err) { return next(err); }
 
       res.render('group', {
         pageTitle: "Inventorization | Group",
         title: "Group",
-        group: group
+        data: results,
       });
-    });
+  });
 });
 
 
