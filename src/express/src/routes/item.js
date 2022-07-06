5edfc812eb06be54c743a8efbc9c0df238557cf0
Yegor Bitensky <yegorbitensky@gmail.com>
2022-07-06 22:52:27 +0300

    Add item data to item page

--- src/express/src/routes/item.js
+++ src/express/src/routes/item.js
@@ -1,13 +1,23 @@
 var express = require('express');
+
+var Item = require('../models/item');
+
 var router = express.Router();
 
 
 router.get('/:itemId/', function(req, res, next) {
   console.log('itemId', req.params.itemId);
-  res.render('item', {
-    pageTitle: "Inventorization | Item",
-    title: "Item"
-  });
+
+  Item.findOne({ _id: req.params.itemId })
+    .exec(function (err, item) {
+      if (err) { return next(err); }
+
+      res.render('item', {
+        pageTitle: "Inventorization | Item",
+        title: "Item",
+        item: item,
+      });
+    });
 });
 
 
