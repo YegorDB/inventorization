8c61852b02f8974d17cb17466f13141e2aae8a13
Yegor Bitensky <yegorbitensky@gmail.com>
2022-12-15 01:12:21 +0300

    Add base groups data api

--- src/express/src/routes/api.js
+++ src/express/src/routes/api.js
@@ -6,23 +6,37 @@ var Item = require('../models/item');
 
 var router = express.Router();
 
+router.get('/groups/', function(req, res, next) {
+  async.parallel({
+    groups: function(callback) {
+      Group.find({group: null}, '_id name').exec(callback);
+    },
+  }, function(err, data) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(data.groups);
+  });
+});
+
 router.get('/groups/:groupId/', function(req, res, next) {
   async.parallel({
-      group: function(callback) {
-          Group.findOne({ _id: req.params.groupId }).exec(callback);
-      },
-      groups: function(callback) {
-          Group.find({ group: req.params.groupId }).exec(callback);
-      },
-      items: function(callback) {
-          Item.find({ group: req.params.groupId }).exec(callback);
-      },
+    group: function(callback) {
+      Group.findOne({ _id: req.params.groupId }).exec(callback);
+    },
+    groups: function(callback) {
+      Group.find({ group: req.params.groupId }).exec(callback);
+    },
+    items: function(callback) {
+      Item.find({ group: req.params.groupId }).exec(callback);
+    },
   }, function(err, data) {
-      if (err) {
-        return next(err);
-      }
+    if (err) {
+      return next(err);
+    }
 
-      res.json(data);
+    res.json(data);
   });
 });
 
