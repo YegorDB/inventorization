35ed68fafb5958f50dab822caa82dbfc48ceaa5f
Yegor Bitensky <yegorbitensky@gmail.com>
2023-01-04 01:17:20 +0300

    Add express env file

--- src/express/src/app.js
+++ src/express/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('secret'));
+app.use(cookieParser(process.env.SECRET));
 
 app.use(sessionMiddleware);
 
