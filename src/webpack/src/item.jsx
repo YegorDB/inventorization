c06d865ab5e92e3df138928be79894fee5416f28
Yegor Bitensky <yegorbitensky@gmail.com>
2023-01-04 19:31:54 +0300

    Remove webpack service

--- src/webpack/src/item.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom/client');
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  ReactDOM
-  .createRoot(document.getElementById('app'))
-  .render(<div>Item page</div>);
-});
