5f9e5cce5acc2f61d0b982d1de18885f0a8a8001
Yegor Bitensky <yegorbitensky@gmail.com>
2024-04-17 00:50:47 +0300

    Add auth login API #1

--- src/django/local_app_users/views.py
+++ src/django/local_app_users/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth import aauthenticate, alogin
 from django.http import JsonResponse
 from django.views import View
 
@@ -7,4 +10,24 @@ class AuthCheck(View):
         user = await request.auser()
         return JsonResponse({
             'success': user.is_authenticated,
+        })
+
+
+class AuthLogin(View):
+    async def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+
+        user = await aauthenticate(
+            request=request,
+            username=data.get('username', ''),
+            password=data.get('password', ''),
+        )
+
+        success = False
+        if user is not None:
+            await alogin(request, user)
+            success = True
+
+        return JsonResponse({
+            'success': success,
         })
--- src/django/main/urls.py
+++ src/django/main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from local_app_users.views import AuthCheck
+from local_app_users.views import AuthCheck, AuthLogin
 
 
 api_urlpatterns = ([
     path('1.0/auth/check/', AuthCheck.as_view(), name='auth-check'),
+    path('1.0/auth/login/', AuthLogin.as_view(), name='auth-login'),
 ], 'api')
 
 
