377dc623c95b1668dccbd82c1896c98616577fdb
Yegor Bitensky <yegorbitensky@gmail.com>
2024-04-17 22:33:50 +0300

    Add get group parents API #1

--- src/django/local_app_inventorization/views.py
+++ src/django/local_app_inventorization/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from asgiref.sync import sync_to_async
 
-# Create your views here.
+from django.db import connection
+from django.http import JsonResponse
+from django.views import View
+
+
+class GetGroupParents(View):
+    async def get(self, request, group_id, *args, **kwargs):
+        rows = await self._get_groups_rows(group_id)
+
+        return JsonResponse([
+            {
+                'id': row_id,
+                'name': name,
+                'group_id': group_id,
+            }
+            for row_id, name, group_id in rows
+        ], safe=False)
+
+    @sync_to_async
+    def _get_groups_rows(self, group_id):
+        sql = '''
+            WITH RECURSIVE groups(id, name) AS (
+                SELECT id, name, group_id
+                FROM local_app_inventorization_group
+                WHERE id = %s
+
+                UNION ALL
+
+                SELECT g.id, g.name, g.group_id
+                FROM groups gs, local_app_inventorization_group g
+                WHERE gs.group_id = g.id
+            )
+            SELECT *
+            FROM groups;
+        '''
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql, [group_id])
+            rows = cursor.fetchall()
+
+        return rows
--- src/django/main/urls.py
+++ src/django/main/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from local_app_inventorization.views import GetGroupParents
 from local_app_users.views import AuthCheck, AuthLogin
 
 
 api_urlpatterns = ([
     path('1.0/auth/check/', AuthCheck.as_view(), name='auth-check'),
     path('1.0/auth/login/', AuthLogin.as_view(), name='auth-login'),
+    path('1.0/groups/<int:group_id>/parents', GetGroupParents.as_view(), name='get-group-parents'),
 ], 'api')
 
 
